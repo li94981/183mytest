d0ce4972d6d09d2732cdb7cc5269bc21a4293206
li94981 <949818467@qq.com>
2020-03-02 20:15:45 +0800

    给视图函数添加冒号，在第四次打印的基础上修改

--- mytest.py
+++ mytest.py
@@ -1,8 +1,8 @@
-def index(request)
+def index(request):
     print('my first test')
 def index1(request):
     print('my second test')
 def index2(request):
     print('my third test')
-def index3(request)
+def index3(request):
     print('my fourth test')
