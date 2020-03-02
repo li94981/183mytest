a2b5d25443a87cf0af5610cb840613356ff16118
li94981 <949818467@qq.com>
2020-03-02 22:14:28 +0800

    在master分支上，测试远程仓库

--- mytest.py
+++ mytest.py
@@ -8,10 +8,11 @@ def index3(request):
     print('my fourth test')
 def index4(request):
     print('my five test')
-def index(request):
+def index7(request):
     print('my eight test')
 def index5(request):
     print('my six test')
 def index6(request):
     print('my seven test')
-
+def index8(request):
+    print('this is a test')
